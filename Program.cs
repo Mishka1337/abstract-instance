cb04e1408e5caf5df83e67506e6884e50d5d0c32
Костылев Михаил <mixakos98@gmail.com>
2020-05-07 17:39:46 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace DigDesHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type abstractClassType = Type.GetType("DigDesHomework.SomeAbstractClass");
+
+            var runtimeTypeHandleType = Type.GetType("System.RuntimeTypeHandle");
+
+            var allocationMethod = runtimeTypeHandleType
+                        .GetMethod("Allocate", BindingFlags.Static | BindingFlags.NonPublic);
+
+            var allocationParameter = abstractClassType.UnderlyingSystemType;
+            var allocationParameters = new object[] { allocationParameter};
+            
+
+            var instance = allocationMethod.Invoke(null, allocationParameters) as SomeAbstractClass;
+
+            instance.Print();
+
+        }
+    }
+
+    abstract class SomeAbstractClass
+    {
+        public void Print()
+        {
+            Console.WriteLine("Hello, DigDes!");
+        }
+    }
+
+}
